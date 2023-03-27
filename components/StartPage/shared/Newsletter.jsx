826f6bc699a72d53d057a00f8e4949dd629d0132
parameter <paer.henriksson@yahoo.com>
2023-03-27 15:43:28 +0200

    everything2

--- components/StartPage/shared/Newsletter.jsx
+++ components/StartPage/shared/Newsletter.jsx
@@ -30,7 +30,7 @@ const Newsletter = ({ source }) => {
     return () => {
       window.removeEventListener('scroll', onScroll);
     };
-  },[setElementOffsetTop, elementOffsetTop, elementHeight, setElementHeight])
+  },[setElementOffsetTop, elementOffsetTop, elementHeight, setElementHeight, setActiveHref, source])
 
   return (
     <div ref={ref} className="relative w-full bg-yellow dark:bg-gray-dark h-[400px] tablet:h-[600px]">
