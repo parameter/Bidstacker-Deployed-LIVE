1d1ed3ecc57a337db298c380a996600fbbd21827
parameter <paer.henriksson@yahoo.com>
2023-04-08 17:30:58 +0200

    altered sitemap

--- pages/_app.js
+++ pages/_app.js
@@ -3,13 +3,12 @@ import { AppProvider } from 'context/app-context';
 import { RequestProvider } from 'context/request-context';
 import { SupplierProvider } from 'context/supplier-context';
 import '@/styles/globals.css';
-
+ 
 export default function PagesApp({ Component, pageProps }) {
-
-  console.log('This is here 1');
-
+  
   return (<>
      {/* <Nav /> */}
      <Component {...pageProps} />
   </>);
 }
+ 
